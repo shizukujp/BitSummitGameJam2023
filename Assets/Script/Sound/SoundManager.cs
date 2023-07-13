0d69fc77de997a9e308ccc5abfbd960691f7d9d6
Kei Yamamoto <hw21a154@oecu.jp>
2023-07-14 00:35:32 +0900

    BGMのクロスフェード作成

--- Assets/Script/Sound/SoundManager.cs
+++ Assets/Script/Sound/SoundManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
@@ -9,9 +12,10 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     AudioSource bgmAudioSource;
     [SerializeField]
+    AudioSource bgmAudioSource2;
+    [SerializeField]
     AudioSource seAudioSource;
 
-    public AudioClip bjm;
     public AudioClip click;
     void Start()
     {
@@ -24,8 +28,7 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        PlayBgm(bjm);
-
+        bgmAudioSource.Play();
         //�V�[����ς��邲�Ƃɐݒ�����Z�b�g����
         //GameObject soundManager = CheckOtherSoundManager();
         //bool checkResult = soundManager != null && soundManager != gameObject;
@@ -49,6 +52,10 @@ public class SoundManager : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             PlaySe(click);
+            if(SceneManager.GetActiveScene().name == "Title")
+            {
+                SwapBGM(bgmAudioSource, bgmAudioSource2);
+            }
         }
     }
 
@@ -97,5 +104,28 @@ public class SoundManager : MonoBehaviour
 
         seAudioSource.PlayOneShot(clip);
     }
+    public void SwapBGM(AudioSource bgm1, AudioSource bgm2)
+    {
+        var fadeOutSource = bgm1;    // 再生中のAudioSourceを取得する
+        var fadeInSource = bgm2; // 再生していないAudioSourceを取得する
+        CrossFadeBGM(fadeInSource, fadeOutSource, 1.0f).Forget();
+    }
 
+    async UniTask CrossFadeBGM(AudioSource fadeInSource, AudioSource fadeOutSource, float duration)
+    {
+        fadeInSource.volume = 0;
+        fadeInSource.Play();
+        float initVol = fadeOutSource.volume;
+        for (float time = 0; time < duration; time += Time.deltaTime * 0.1f)
+        {
+            fadeInSource.volume = Mathf.Lerp(0, 0.3f, time / duration);
+            fadeOutSource.volume = Mathf.Lerp(initVol, 0, (time / duration) * 2f);
+
+            await UniTask.WaitForEndOfFrame();
+            /* ~中断チェックは省略~ */
+        }
+
+        fadeInSource.volume = 0.3f;
+        fadeOutSource.volume = 0;
+    }
 }
