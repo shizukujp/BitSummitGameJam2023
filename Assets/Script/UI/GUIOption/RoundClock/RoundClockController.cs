a8330ebf78375c094c70c38edb8db50ef3360ee5
LAPTOP-KIYOHARU\kiyoharu <1905088203@qq.com>
2023-07-13 23:08:29 +0900

    ラウンドアニメーション

---
+++ Assets/Script/UI/GUIOption/RoundClock/RoundClockController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundClockController : MonoBehaviour
+{
+    public void PlayRoundClockAnim(float time)
+    {
+        Debug.Log(time);
+    }
+}
